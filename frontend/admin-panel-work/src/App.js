64166733f3051259414d8d5ab32386a292e1f617
vansh <vanshsin784@gmail.com>
2024-09-03 22:25:56 +0530

    Adding new admin panel

--- frontend/admin-panel-work/src/App.js
+++ frontend/admin-panel-work/src/App.js
@@ -1,7 +1,7 @@
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'remixicon/fonts/remixicon.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.min.js'
+import 'bootstrap/dist/js/bootstrap.min'
 import './App.css';
 import Header from './components/Header/Header';
 import Sidebar from './components/Sidebar/Sidebar';
